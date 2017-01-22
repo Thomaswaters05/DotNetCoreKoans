fdea17bb19e8d7bee5fcc611ebe6ea22eb47e7e8
Tommy Waters <Thomas.Waters05@gmail.com>
2017-01-22 16:56:08 -0600

    almost completed

--- Koans/AboutMethods.cs
+++ Koans/AboutMethods.cs
@@ -40,19 +40,19 @@ namespace DotNetCoreKoans.Koans
         [Step(1)]
         public void ExtensionMethodsShowUpInTheCurrentClass()
         {
-            Assert.Equal(FILL_ME_IN, this.HelloWorld());
+            Assert.Equal("Hello!", this.HelloWorld());
         }
 
         [Step(2)]
         public void ExtensionMethodsWithParameters()
         {
-            Assert.Equal(FILL_ME_IN, this.SayHello("Cory"));
+            Assert.Equal("Hello, Cory!", this.SayHello("Cory"));
         }
 
         [Step(3)]
         public void ExtensionMethodsWithVariableParameters()
         {
-            Assert.Equal(FILL_ME_IN, this.MethodWithVariableArguments("Cory", "Will", "Corey"));
+            Assert.Equal(new string[]{"Cory", "Will", "Corey"} , this.MethodWithVariableArguments("Cory", "Will", "Corey"));
         }
 
         //Extension methods can extend any class my referencing 
@@ -62,7 +62,7 @@ namespace DotNetCoreKoans.Koans
         [Step(4)]
         public void ExtendingCoreClasses()
         {
-            Assert.Equal(FILL_ME_IN, "Cory".SayHi());
+            Assert.Equal("Hi, Cory", "Cory".SayHi());
         }
 
         //Of course, any of the parameter things you can do with 
